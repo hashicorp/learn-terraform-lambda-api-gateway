5ad4caa7390d3bcae080009326a9df4886314bec
Robin Norwood <rnorwood@hashicorp.com>
2021-07-12 10:50:38 -0500

    Include initial function code

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -1,1 +1,16 @@
 // Lambda function code
+
+module.exports.handler = async (event) => {
+console.log('Event: ', event)
+  let responseMessage = 'Hello, World!';
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      message: responseMessage,
+    }),
+  }
+}
