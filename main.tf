fa39f68523ef0209c5ab2815a8d4cf24ad7b691b
Robin Norwood <rnorwood@hashicorp.com>
2022-02-02 09:47:19 -0600

    Update to AWS provider 4

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.48.0"
+      version = "~> 4.0.0"
     }
     random = {
       source  = "hashicorp/random"
