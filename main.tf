eb8f9a8629a86bfd4c95dde08035493e934399a9
pablo dias <pablohenrique.mg@gmail.com>
2022-03-22 12:22:31 -0300

    Error: Value for unconfigurable attribute

--- main.tf
+++ main.tf
@@ -29,6 +29,5 @@ resource "random_pet" "lambda_bucket_name" {
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = random_pet.lambda_bucket_name.id
 
-  acl           = "private"
   force_destroy = true
 }
